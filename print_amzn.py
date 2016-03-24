5b85843003927d4fdafd59029669f2a01617d2d4
Avneet Kaur <Avnikaur@users.noreply.github.com>
2016-03-24 12:54:55 -0400

    Update print_amzn.py

--- print_amzn.py
+++ print_amzn.py
@@ -1,4 +1,4 @@
-# this code flattens Amazon product reviews into csv file
+# This code flattens Amazon product reviews into csv file
 import json
 import os
 import sys
@@ -11,9 +11,10 @@ amazon_reviews ="/Volumes/My Passport/BU - Data warehousing/datasets/AmazonRevie
 top_level_keys =[u'Reviews', u'ProductInfo']
 review_keys =[u'Author', u'ReviewID', u'Overall', u'Content', u'Title', u'Date']
 product_keys =[u'Price', u'ProductID', u'Features', u'ImgURL', u'Name']
-# remember for each product there is many review - 1 - many relation
 
-#print columns first
+# For each product there are many reviews ie. 1 to many relationship
+
+#Print columns first
 category_list=[dir.strip() for dir in os.listdir(amazon_reviews) if dir != '.DS_Store']
 log_file = open("print_amzn.log","w")
 exception_log = open("print_amzn_except.log","w")
@@ -23,17 +24,17 @@ for category in category_list:
  file_list = [fl for fl in os.listdir(full_dir_path) if fl != '.DS_Store']
  for fl in file_list:
     try:
-     #print "@".join(product_keys) + "@" + "@".join(review_keys)
+     
      with open(full_dir_path + "/" + fl) as f:
       data = f.read()
       jsondata = json.loads(data)
 
 
       for row in jsondata:
-          # pull the product info first
-          #notice the use of str to deal with None return values in case lookup of column fails
+          # Pull the product info first
+          #Notice the use of string to deal with None return values in case lookup of column fails
           product_info="@".join([jsondata['ProductInfo'][key] or '' for key in product_keys])
-     # now for each review - remember there is many
+     
           number_reviews = len(jsondata['Reviews'])
           for i in range(number_reviews):
               review = jsondata['Reviews'][i]
